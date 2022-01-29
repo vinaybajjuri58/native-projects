af3e638a62bc48a3e04a3c246d74e9ed322a990b
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2022-01-30 00:01:36 +0530

    Hello World App

--- App.js
+++ App.js
@@ -6,37 +6,13 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const incrementHandler = () => {
-    setCounter(initial => initial + 1);
-  };
-  const decrementHandler = () => {
-    setCounter(initial => initial - 1);
-  };
   return (
     <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Counter Application</Text>
-      <Text style={styles.sectionSubTitle}>Counter Value : {counter}</Text>
-      <View style={styles.buttonCountainer}>
-        <Button
-          title="Decrement"
-          color="red"
-          onPress={decrementHandler}
-          style={styles.buttonStyle}
-          accessibilityLabel="DECREMENT"
-        />
-        <Button
-          title="Increment"
-          color="green"
-          onPress={incrementHandler}
-          style={styles.buttonStyle}
-          accessibilityLabel="INCREMENT"
-        />
-      </View>
+      <Text style={styles.sectionTitle}>Hello World</Text>
     </View>
   );
 };
@@ -52,19 +28,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
     padding: 4,
-  },
-  sectionSubTitle: {
-    fontSize: 18,
-    fontWeight: '400',
-    padding: 4,
-  },
-  buttonCountainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  buttonStyle: {
-    padding: 1,
-    margin: 2,
   },
 });
 
